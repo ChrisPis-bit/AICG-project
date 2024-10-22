39bd12f286eaacb8d6d70dda55ee3e9e79616bd8
ricoround <ronderockrico@gmail.com>
2024-10-22 20:57:36 +0200

    geprobeerd maar lukt niet

---
+++ AICG-project/Assets/TTS.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+namespace Voxell.Speech.TTS.Demo
+{
+  public class TTSDemo : MonoBehaviour
+  {
+    public TMP_InputField inputField;
+    public TextToSpeech textToSpeech;
+
+    // public void SpeakInput() => textToSpeech.Speak(inputField.text);
+
+    void OnDisable()
+      => textToSpeech.Dispose();
+
+    void Start() {
+        GetComponent<TextToSpeech>().Speak("hello this is a test");
+    }
+  }
+}
