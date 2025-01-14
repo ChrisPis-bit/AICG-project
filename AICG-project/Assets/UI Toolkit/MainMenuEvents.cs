1df016d9b372937774ec3c46a3190b082e5b0f68
ricoround <ronderockrico@gmail.com>
2025-01-14 19:52:27 +0100

    wip chatcanvas ui

--- AICG-project/Assets/UI Toolkit/MainMenuEvents.cs
+++ AICG-project/Assets/UI Toolkit/MainMenuEvents.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using FMODUnity;
 
 public class MainMenuEvents : MonoBehaviour
 {
@@ -12,7 +13,10 @@ public class MainMenuEvents : MonoBehaviour
     private Button _exitButton;
     private List<Button> _menuButtons;
 
-    private SceneManager _sceneManager;
+    // slider
+    [SerializeField] private float _volume = 0.8f;
+    private Slider _slider;
+
 
     private void Awake(){
         _document = GetComponent<UIDocument>();
@@ -20,6 +24,13 @@ public class MainMenuEvents : MonoBehaviour
         _startButton.RegisterCallback<ClickEvent>(OnStartGameClick);
         _exitButton = _document.rootVisualElement.Q<Button>("ExitButton") as Button;
         _exitButton.RegisterCallback<ClickEvent>(onGameExitClick);
+
+        // slider
+        _slider = _document.rootVisualElement.Q<Slider>("VolumeSlider") as Slider;
+        _slider.RegisterValueChangedCallback(OnSliderChange);
+        _slider.value = _volume;
+
+
         _menuButtons = _document.rootVisualElement.Query<Button>().ToList();
         for (int i = 0; i < _menuButtons.Count; i++){
             _menuButtons[i].RegisterCallback<ClickEvent>(OnMenuButtonClick);
@@ -42,5 +53,12 @@ public class MainMenuEvents : MonoBehaviour
 
     private void OnMenuButtonClick(ClickEvent evt){
         Debug.Log("Menu Button Clicked");
+    }
+
+    private void OnSliderChange(ChangeEvent<float> evt){
+        Debug.Log("Slider Value Changed: " + evt.newValue);
+        // change fmod parameter 'MasterVolume'
+        var log = RuntimeManager.StudioSystem.setParameterByName("MasterVolume", evt.newValue);
+        Debug.Log("FMOD set parameter: " + log);
     }
 }
