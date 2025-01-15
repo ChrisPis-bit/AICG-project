0706fc68d4a72e00592cc470054ccf4dc520609f
Chris Lokhorst <chris.lokhorst@hva.nl>
2025-01-15 21:56:40 +0100

    Added AI Audio

---
+++ AICG-project/Assets/Scripts/AISoundPlayer.cs
@@ -0,0 +1,37 @@
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISoundPlayer : MonoBehaviour
+{
+    [SerializeField] private FMODUnity.EventReference _talkEvent;
+    [SerializeField] private ChatHandler _chatHandler;
+
+    private FMOD.Studio.EventInstance instance;
+
+    private void OnEnable()
+    {
+        instance = RuntimeManager.CreateInstance(_talkEvent);
+        _chatHandler.onStateChange += OnStateChange;
+    }
+
+    private void OnDisable()
+    {
+        instance.release();
+        _chatHandler.onStateChange -= OnStateChange;
+    }
+
+    private void OnStateChange(AIStates state)
+    {
+        switch (state)
+        {
+            case AIStates.Talking:
+                instance.start();
+                break;
+            default:
+                instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                break;
+        }
+    }
+}
